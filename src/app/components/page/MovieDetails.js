c6509010878525483f89fc9c3975a3849a4cc791
ghostanza <friedcaligari@gmail.com>
2018-05-02 15:51:15 -0400

    add basic addToLibrary function to add movie item to user's firebase DB path

--- src/app/components/page/MovieDetails.js
+++ src/app/components/page/MovieDetails.js
@@ -1,6 +1,23 @@
 import React from 'react';
 
 export default class MovieDetails extends React.Component {
+  constructor(p){
+    super(p);
+    this.state = {
+      db: p.firebase.database().ref(`users/${p.uid}/library/${p.data.id}`)
+    }
+  }
+  addToLibrary(e){
+    if([...e.target.classList].includes('loading')){
+      return;
+    }
+    else{
+      e.target.classList.add('loading');
+      let added = new Date();
+      added = added.toLocaleDateString();
+      this.state.db.set({added});
+    }
+  }
   render() {
     let data = this.props.data;
     return(
@@ -12,6 +29,7 @@ export default class MovieDetails extends React.Component {
             <h2>{data.title || ''}</h2>
             {data.formatted_release_date ? (<p>{data.formatted_release_date.string}</p>) : ''}
             {data.overview ? (<p>{data.overview}</p>) : ''}
+            <div onClick={this.addToLibrary.bind(this)}>Check In</div>
             <div onClick={this.props.back}>Back</div>
           </div>
         ) : (<p>Could not find data</p>)
