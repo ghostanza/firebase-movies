3476de143086ce4111892d79cf1f768d73d6626a
ghostanza <friedcaligari@gmail.com>
2018-05-02 16:19:24 -0400

    check if user already has movie in their library before showing Check In button

--- src/app/components/page/MovieDetails.js
+++ src/app/components/page/MovieDetails.js
@@ -4,8 +4,14 @@ export default class MovieDetails extends React.Component {
   constructor(p){
     super(p);
     this.state = {
-      db: p.firebase.database().ref(`users/${p.uid}/library/${p.data.id}`)
+      db: p.firebase.database().ref(`users/${p.uid}/library/${p.data.id}`),
+      existingData: ''
     }
+    this.state.db.on('value', (snapshot) => {
+      if(snapshot.val()){
+        this.setState((p) => ({...p, existingData: snapshot.val()}));
+      }
+    });
   }
   addToLibrary(e){
     if([...e.target.classList].includes('loading')){
@@ -29,7 +35,7 @@ export default class MovieDetails extends React.Component {
             <h2>{data.title || ''}</h2>
             {data.formatted_release_date ? (<p>{data.formatted_release_date.string}</p>) : ''}
             {data.overview ? (<p>{data.overview}</p>) : ''}
-            <div onClick={this.addToLibrary.bind(this)}>Check In</div>
+            {!this.state.existingData ? (<div onClick={this.addToLibrary.bind(this)}>Check In</div>) : (<div>Added on: {this.state.existingData.added}</div>)}
             <div onClick={this.props.back}>Back</div>
           </div>
         ) : (<p>Could not find data</p>)
